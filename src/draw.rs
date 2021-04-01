2b867ce40fcc12f60bb9fe2d6b1637cd6921794a
Mike Walters <mike@flomp.net>
2021-04-01 13:10:56 +0100

    Optimise LUT
    A lot of time was being spent in RGBColor.int_x(), so this change folds
    that into the LUT.

--- src/draw.rs
+++ src/draw.rs
@@ -33,10 +33,11 @@ pub fn colormaps() -> HashMap<&'static str, Box<dyn MyColorMap>> {
     maps
 }
 
-fn build_lut(colormap: &Box<dyn MyColorMap>) -> Vec<RGBColor> {
+fn build_lut(colormap: &Box<dyn MyColorMap>) -> Vec<[u8; 3]> {
     let mut lut = Vec::with_capacity(u16::MAX as usize + 1);
     for i in 0..u16::MAX as usize + 1 {
-        lut.push(colormap.transform_single(i as f64 / u16::MAX as f64));
+        let value = colormap.transform_single(i as f64 / u16::MAX as f64);
+        lut.push([value.int_r(), value.int_g(), value.int_b()]);
     }
     lut
 }
@@ -53,13 +54,14 @@ pub fn draw_image(record_collection: &RecordCollection, output_path: &str, setti
     let lut = build_lut(settings.colormap);
     let range = settings.power_max - settings.power_min;
     let scale = (lut.len()-1) as f32 / range;
+
     for record in &rc.records {
         let mut x = ((record.freq_low - rc.freq_low) as f32 / rc.freq_step) as u32;
         let y = rc.timestamps[&NaiveDateTime::new(record.date, record.time)];
         for sample in &record.samples {
             let scaled_pixel = (sample - settings.power_min) * scale;
             let value = lut[(scaled_pixel as usize).clamp(0, lut.len()-1)];
-            img.put_pixel(x, y, Rgb([value.int_r(), value.int_g(), value.int_b()]));
+            img.put_pixel(x, y, Rgb(value));
             x += 1
         }
     }
