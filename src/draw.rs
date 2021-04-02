c1f577c52d4962612990c83a057f885a5e221811
Mike Walters <mike@flomp.net>
2021-04-02 12:57:06 +0100

    Fix rounding issue

--- src/draw.rs
+++ src/draw.rs
@@ -5,9 +5,9 @@ use chrono::naive::NaiveDateTime;
 use image::{Rgb, RgbImage};
 use imageproc::drawing::{draw_filled_rect_mut, draw_text_mut};
 use imageproc::rect::Rect;
+use plotters::{chart::ChartBuilder, prelude::{BitMapBackend, IntoDrawingArea}, style::{AsRelative, RGBColor, WHITE}};
 use rusttype::{Font, Scale};
 pub use scarlet::colormap::{ColorMap, ListedColorMap};
-use scarlet::color::RGBColor;
 
 use super::csv::RecordCollection;
 
@@ -19,11 +19,11 @@ pub struct DrawSettings<'a> {
 }
 
 pub trait MyColorMap {
-    fn transform_single(&self, _: f64) -> RGBColor;
+    fn transform_single(&self, _: f64) -> scarlet::color::RGBColor;
 }
 
 impl MyColorMap for ListedColorMap {
-    fn transform_single(&self, x: f64) -> RGBColor {
+    fn transform_single(&self, x: f64) -> scarlet::color::RGBColor {
         scarlet::colormap::ColorMap::transform_single(self, x)
     }
 }
@@ -37,11 +37,11 @@ pub fn colormaps() -> HashMap<&'static str, Box<dyn MyColorMap>> {
     maps
 }
 
-fn build_lut(colormap: &Box<dyn MyColorMap>) -> Vec<[u8; 3]> {
+fn build_lut(colormap: &Box<dyn MyColorMap>) -> Vec<plotters::style::RGBColor> {
     let mut lut = Vec::with_capacity(u16::MAX as usize + 1);
     for i in 0..u16::MAX as usize + 1 {
         let value = colormap.transform_single(i as f64 / u16::MAX as f64);
-        lut.push([value.int_r(), value.int_g(), value.int_b()]);
+        lut.push(plotters::style::RGBColor{0: value.int_r(), 1: value.int_g(), 2: value.int_b()});
     }
     lut
 }
@@ -53,19 +53,31 @@ pub fn draw_image(record_collection: &RecordCollection, output_path: &str, setti
     let height = rc.timestamps.len() as u32;
     println!("Width: {} Height: {}", width, height);
 
-    let mut img = RgbImage::new(width, height);
+
+    let b = BitMapBackend::new(output_path, (width, height))
+        .into_drawing_area()
+        .margin(20, 20, 20, 20);
+    b.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&b)
+        .margin(1)
+        .caption("test", ("sans-serif", (10.percent_height())))
+        .set_label_area_size(plotters::prelude::LabelAreaPosition::Left, (5i32).percent_width())
+        .set_label_area_size(plotters::prelude::LabelAreaPosition::Top, (5i32).percent_height())
+        .build_cartesian_2d(0..width, 0..height)?;
+    chart.configure_mesh().draw()?;
+    let pa = chart.plotting_area();
 
     let lut = build_lut(settings.colormap);
     let range = settings.power_max - settings.power_min;
     let scale = (lut.len()-1) as f32 / range;
 
     for record in &rc.records {
-        let mut x = ((record.freq_low - rc.freq_low) as f32 / rc.freq_step) as u32;
+        let mut x = (((record.freq_low - rc.freq_low) as f32 / rc.freq_step) + 0.5) as u32;
         let y = rc.timestamps[&NaiveDateTime::new(record.date, record.time)];
         for sample in &record.samples {
             let scaled_pixel = (sample - settings.power_min) * scale;
-            let value = lut[(scaled_pixel as usize).clamp(0, lut.len()-1)];
-            img.put_pixel(x, y, Rgb(value));
+            let value = &lut[(scaled_pixel as usize).clamp(0, lut.len()-1)];
+            pa.draw_pixel((x, y), value)?;
             x += 1
         }
     }
@@ -91,11 +103,11 @@ pub fn draw_image(record_collection: &RecordCollection, output_path: &str, setti
             let y = rc.timestamps[ts];
             let text = &ts.to_string();
             let h = scale.y as u32;
-            draw_filled_rect_mut(&mut img, Rect::at(0, y as i32).of_size(20, 2), white);
-            draw_text_mut(&mut img, black, x+2, y-h+2, scale, &font, text);
-            draw_text_mut(&mut img, white, x, y-h, scale, &font, text);
+            //draw_filled_rect_mut(&mut img, Rect::at(0, y as i32).of_size(20, 2), white);
+            //draw_text_mut(&mut img, black, x+2, y-h+2, scale, &font, text);
+            //draw_text_mut(&mut img, white, x, y-h, scale, &font, text);
         }
     }
-    img.save(output_path)?;
+    //img.save(output_path)?;
     Ok(())
 }
